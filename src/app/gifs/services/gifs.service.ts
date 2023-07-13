336f1c9788c19c0e0fd984c6f454055245b7caf9
Wilder Canales <saske21x@gmail.com>
2023-07-13 15:02:31 -0500

    Mejora de la app

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,10 @@ export class GifsService {
   private apiKey: string = 'Zkj0Ajueujpza0I0l94VWYQ4Va8KMitY';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Tags History');
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -33,6 +36,15 @@ export class GifsService {
 
   private saveLocalStorage(): void {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage():void{
+    if(! localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if ( this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
